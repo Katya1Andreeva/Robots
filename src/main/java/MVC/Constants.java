c5485132186db179d89ec1cb299574cfc426f934
Katya1Andreeva <91565371+Katya1Andreeva@users.noreply.github.com>
2023-05-26 16:38:19 +0300

    add constants

--- src/main/java/MVC/Constants.java
+++ src/main/java/MVC/Constants.java
@@ -5,4 +5,10 @@ public class Constants {
     public static final double MAX_VELOCITY = 0.1;
     public static final double MAX_ANGULAR_VELOCITY = 0.001;
     public static final double DURATION = 10;
+    public static final int BODY_WIDTH = 10;
+    public static final int BODY_HEIGHT = 30;
+    public static final int GAME_WINDOW_WIDTH = 400;
+    public static final int GAME_WINDOW_HEIGHT = 400;
+    public static final int EYE_DIAMETER = 5;
+    public static final int EYE_SHIFT = 10;
 }
